15fd2e2b3e6bf6c453602bfc9a3de52681bc2ab1
flores-o <oana.florescu28@gmail.com>
2020-10-26 22:26:37 +0000

    followed https://render.com/docs/deploy-phoenix

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :mentorship, MentorshipWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "evening-caverns-78277.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]], 
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
